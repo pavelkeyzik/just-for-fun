648de1565e5d7a6ec2cfa34821e14c0501aca99b
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-09-23 21:08:19 +0300

    Added useAddPlaceForm hook

---
+++ src/hooks/add-place-form.js
@@ -0,0 +1,124 @@
+import { useReducer } from 'react';
+
+/**
+ * @typedef {Object} AddPlaceForm
+ * @property {string} name - Place's name
+ * @property {string} address - Place's address
+ * @property {number} lat - Place's latitude coordinate
+ * @property {number} lng - Place's longitude coordinate
+ */
+
+/**
+ * @typedef {Object} Action
+ * @property {string} type - Type of action
+ * @property {*} payload - Any data that you wanna send
+ */
+
+const actions = {
+  SET_NAME: 'SET_NAME',
+  SET_ADDRESS: 'SET_ADDRESS',
+  SET_LATITUDE: 'SET_LATITUDE',
+  SET_LONGITUDE: 'SET_LONGITUDE',
+};
+
+/**
+ * Inital state for reducer
+ *
+ * @type {AddPlaceForm}
+ */
+const initialState = {
+  name: '',
+  address: '',
+  lat: 0.0,
+  lng: 0.0,
+};
+
+/**
+ *
+ * @param {AddPlaceForm} state - Current state
+ * @param {Action} action - Action to upate state
+ *
+ * @returns {AddPlaceForm}
+ */
+function reducer(state, action) {
+  switch (action.type) {
+    case actions.SET_NAME:
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case actions.SET_ADDRESS:
+      return {
+        ...state,
+        address: action.payload,
+      };
+    case actions.SET_LATITUDE:
+      return {
+        ...state,
+        lat: action.payload,
+      };
+    case actions.SET_LONGITUDE:
+      return {
+        ...state,
+        lng: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+/**
+ * Hook for `Add new place` form
+ *
+ * @param {AddPlaceForm} data - Information about place
+ */
+export function useAddPlaceForm() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  /**
+   * Function that dispatch action to change place name
+   *
+   * @param {strign} value - New value of place name
+   */
+  function setName(value) {
+    dispatch({ type: actions.SET_NAME, payload: value });
+  }
+
+  /**
+   * Function that dispatch action to change place address
+   *
+   * @param {strign} value - New value of place address
+   */
+  function setAddress(value) {
+    dispatch({ type: actions.SET_ADDRESS, payload: value });
+  }
+
+  /**
+   * Function that dispatch action to change place's latitude coordinate
+   *
+   * @param {strign} value - New value of latitude coordinate
+   */
+  function setLatitude(value) {
+    dispatch({ type: actions.SET_LATITUDE, payload: value });
+  }
+
+  /**
+   * Function that dispatch action to change place's latitude coordinate
+   *
+   * @param {strign} value - New value of latitude coordinate
+   */
+  function setLongitude(value) {
+    dispatch({ type: actions.SET_LONGITUDE, payload: value });
+  }
+
+  return {
+    name: state.name,
+    address: state.address,
+    lat: state.lat,
+    lng: state.lng,
+    setName,
+    setAddress,
+    setLatitude,
+    setLongitude,
+  };
+}
--- src/pages/home/AddPlaceForm.js
+++ src/pages/home/AddPlaceForm.js
@@ -4,7 +4,64 @@ import { Button } from '../../components/Button';
 import { InputLabel } from '../../components/InputLabel';
 import { Input } from '../../components/Input';
 
+import { useAddPlaceForm } from '../../hooks/add-place-form';
+
 export function AddPlaceForm() {
+  const {
+    name,
+    address,
+    lat,
+    lng,
+    setName,
+    setAddress,
+    setLatitude,
+    setLongitude,
+  } = useAddPlaceForm();
+
+  function handleAddNewPlace(event) {
+    event.preventDefault();
+  }
+
+  /**
+   * Function that changes value of name
+   *
+   * @param {Event} event
+   */
+  function handleChangeName(event) {
+    const value = event.currentTarget.value;
+    setName(value);
+  }
+
+  /**
+   * Function that changes value of the address
+   *
+   * @param {Event} event
+   */
+  function handleChangeAddress(event) {
+    const value = event.currentTarget.value;
+    setAddress(value);
+  }
+
+  /**
+   * Function that changes value of the latitude coordinate
+   *
+   * @param {Event} event
+   */
+  function handleChangeLatitude(event) {
+    const value = event.currentTarget.value;
+    setLatitude(value);
+  }
+
+  /**
+   * Function that changes value of the longitude coordinate
+   *
+   * @param {Event} event
+   */
+  function handleChangeLongitude(event) {
+    const value = event.currentTarget.value;
+    setLongitude(value);
+  }
+
   return (
     <div
       style={{
@@ -20,22 +77,43 @@ export function AddPlaceForm() {
           display: 'grid',
           gridGap: 20,
         }}
+        onSubmit={handleAddNewPlace}
       >
         <InputLabel>
           <span>Place name:</span>
-          <Input type="text" name="name" />
+          <Input
+            value={name}
+            type="text"
+            name="name"
+            onChange={handleChangeName}
+          />
         </InputLabel>
         <InputLabel>
           <span>Place address:</span>
-          <Input type="text" name="address" />
+          <Input
+            value={address}
+            type="text"
+            name="address"
+            onChange={handleChangeAddress}
+          />
         </InputLabel>
         <InputLabel>
           <span>Latitude coordinate:</span>
-          <Input type="number" name="lat" />
+          <Input
+            value={lat}
+            type="number"
+            name="lat"
+            onChange={handleChangeLatitude}
+          />
         </InputLabel>
         <InputLabel>
           <span>Longitude coordinate:</span>
-          <Input type="number" name="lng" />
+          <Input
+            value={lng}
+            type="number"
+            name="lng"
+            onChange={handleChangeLongitude}
+          />
         </InputLabel>
         <Button type="submit" style={{ marginTop: 20 }}>
           Add new place
