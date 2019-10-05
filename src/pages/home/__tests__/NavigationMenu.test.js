83530dd1dc70557d2c683782952a0f5712f2004f
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-05 13:57:23 +0000

    Added tests for NavigationMenu component

---
+++ src/pages/home/__tests__/NavigationMenu.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { NavigationMenu } from '../NavigationMenu';
+import { projectName, projectRepository } from '../../../config';
+
+jest.mock('../../../config', () => {
+	return {
+		projectName: 'PROJECT_NAME',
+		projectRepository: 'PROJECT_LINK',
+	};
+})
+
+test('Title should be equal to projectName field in config', () => {
+	const container = document.createElement('div');
+	ReactDOM.render(<NavigationMenu />, container);
+	const title = container.querySelector('.navbar-brand');
+	
+	expect(title.textContent).toBe(projectName);
+})
+
+test('Link to GitHub repo should be equal to projectRepository field in config', () => {
+	const container = document.createElement('div');
+	ReactDOM.render(<NavigationMenu />, container);
+	const links = container.querySelectorAll('.NavigationMenu__container li a');
+	const repoLinkElement = links[links.length - 1];
+
+	expect(repoLinkElement.getAttribute('href')).toBe(projectRepository);
+})
