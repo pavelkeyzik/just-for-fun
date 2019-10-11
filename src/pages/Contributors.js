1aa06445318c993f1c42bcae320668db1931eb6c
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-11 18:16:40 +0300

    Fixed linter problems

--- src/pages/Contributors.js
+++ src/pages/Contributors.js
@@ -5,12 +5,12 @@ import { ContributorsTable } from './contributors/ContributorsTable';
 import './Contributors.css';
 
 function Contributors() {
-    return (
-        <main className="Contributors__main">
-            <h2 className="Contributors__section-title">Contributors</h2>
-            <ContributorsTable />
-        </main>
-    );
+  return (
+    <main className="Contributors__main">
+      <h2 className="Contributors__section-title">Contributors</h2>
+      <ContributorsTable />
+    </main>
+  );
 }
 
 export default Contributors;
--- src/pages/contributors/ContributorsTable.js
+++ src/pages/contributors/ContributorsTable.js
@@ -4,25 +4,37 @@ import { Table } from 'reactstrap';
 import './ContributorsTable.css';
 
 export function ContributorsTable() {
-    return (
-        <Table className="ContributorsTable__table">
-            <thead>
-                <th>#</th>
-                <th>Username</th>
-                <th>GitHub</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>pavelkeyzik </td>
-                    <td> <a href="https://github.com/pavelkeyzik"> https://github.com/pavelkeyzik</a></td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Hritik Kumar </td>
-                    <td> <a href="https://github.com/Eagle732"> https://github.com/Eagle732</a></td>
-                </tr>
-            </tbody>
-        </Table>
-    );
+  return (
+    <Table className="ContributorsTable__table">
+      <thead>
+        <th>#</th>
+        <th>Username</th>
+        <th>GitHub</th>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>pavelkeyzik </td>
+          <td>
+            {' '}
+            <a href="https://github.com/pavelkeyzik">
+              {' '}
+              https://github.com/pavelkeyzik
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Hritik Kumar </td>
+          <td>
+            {' '}
+            <a href="https://github.com/Eagle732">
+              {' '}
+              https://github.com/Eagle732
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
 }
