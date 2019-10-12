fd23eb5440e9160ee0b6b8fa615b6096978c7ba9
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-12 19:45:49 +0000

    Added admin page route

--- src/config.js
+++ src/config.js
@@ -12,4 +12,5 @@ export const routes = {
   home: '/',
   places: '/places',
   contributors: '/contributors',
+	admin: '/admin',
 };
---
+++ src/pages/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Admin() {
+	return (
+		<div>
+			<h2>Hi! This page is just for admin. Please, leave it if you are not.</h2>
+		</div>
+	);
+}
+
+export default Admin;
