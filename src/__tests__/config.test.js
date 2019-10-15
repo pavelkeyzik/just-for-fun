678918f105e2958543e4aee76d0ae60d4cb5b394
Alkis <pgiouvanelis@gmail.com>
2019-10-15 14:45:15 +0200

    Simple unit tests for configuration file.

--- src/__tests__/config.test.js
+++ src/__tests__/config.test.js
@@ -1,4 +1,4 @@
-import { routes } from '../config';
+import { routes, apiBase, projectRepository, projectName, projectName_Accessible } from '../config'; 
 
 describe('Configuration file', () => {
   test('check routes object', () => {
@@ -11,5 +11,21 @@ describe('Configuration file', () => {
     };
 
     expect(routes).toEqual(objectToCheck);
+  });
+
+  test('check api base url', () => {
+    expect(apiBase).toEqual('http://localhost:3500/');
+  });
+
+  test('check project repository url', () => {
+    expect(projectRepository).toEqual('https://github.com/pavelkeyzik/just-for-fun');
+  });
+
+  test('check project name', () => {
+    expect(projectName).toEqual('JUSTFORFUN');
+  });
+
+  test('check accessible project name', () => {
+    expect(projectName_Accessible).toEqual('Just for fun');
   });
 });
