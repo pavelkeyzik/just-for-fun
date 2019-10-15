b0b70f19b8475f6c3068657a6f901de220a0e393
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-15 09:49:30 +0300

    Added Header component for Dashboard with ability to go to the home page of our site

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import { Header } from './components/Header';
 
 function Dashboard() {
-  return <div>Dashboard Page</div>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <Header />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm="4" xl="2">
+          Navigation menu
+        </Col>
+        <Col sm="8" xl="10">
+          Content
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Dashboard;
