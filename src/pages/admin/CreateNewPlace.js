72344e9d969e922d6f896b6201b023257b127879
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-22 17:19:18 +0300

    Added page to create new place

---
+++ src/pages/admin/CreateNewPlace.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CreateNewPlace() {
+  return (
+    <React.Fragment>
+      <div className="Page__short-info">
+        <h2>Create new place</h2>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default CreateNewPlace;
