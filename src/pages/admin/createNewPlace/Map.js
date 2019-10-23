0894a8e29f44a1219cccd240177424d4b7ca355b
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-23 18:17:05 +0300

    Added mutation to create new place

--- src/pages/admin/createNewPlace/Map.js
+++ src/pages/admin/createNewPlace/Map.js
@@ -6,7 +6,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { mapBoxAPIAccessToken } from '../../../config';
 import { Pin } from '../../../components/Pin';
 
-export function Map() {
+export function Map({ markerCoordinates, onChangeMarkerPosition }) {
   const [viewport, setViewport] = useState({
     width: '100%',
     height: 400,
@@ -14,17 +14,13 @@ export function Map() {
     longitude: 27.545068,
     zoom: 11,
   });
-  const [markerCoordinates, setPinCoordinates] = useState({
-    latitude: 53.888019427799776,
-    longitude: 27.544588765497792,
-  });
 
   function handleViewportChange(updatedViewport) {
     setViewport(updatedViewport);
   }
 
   function handleChangeMarkerPosition(updatedPosition) {
-    setPinCoordinates({
+    onChangeMarkerPosition({
       latitude: updatedPosition.lngLat[1],
       longitude: updatedPosition.lngLat[0],
     });
