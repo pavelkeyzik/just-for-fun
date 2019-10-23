d7604cac0e7561803eb4486384b2472de55a6006
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-23 17:47:53 +0300

    Added Marker on Map and ability to change position

---
+++ src/components/Pin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ReactComponent as PinIcon } from '../images/pin.svg';
+
+import './Pin.css';
+
+export function Pin() {
+  return <PinIcon className="Pin__icon" />;
+}
