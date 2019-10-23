7a809b55c80c5041049b06da3faf096568b2910e
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-23 17:47:53 +0300

    Added Marker on Map and ability to change position

--- src/pages/admin/createNewPlace/Map.js
+++ src/pages/admin/createNewPlace/Map.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
 import 'mapbox-gl/dist/mapbox-gl.css';
+
 import { mapBoxAPIAccessToken } from '../../../config';
+import { Pin } from '../../../components/Pin';
 
 export function Map() {
   const [viewport, setViewport] = useState({
     width: '100%',
     height: 400,
-    latitude: 53.89955,
-    longitude: 27.558355,
+    latitude: 53.8885,
+    longitude: 27.545068,
     zoom: 11,
+  });
+  const [markerCoordinates, setPinCoordinates] = useState({
+    latitude: 53.888019427799776,
+    longitude: 27.544588765497792,
   });
 
   function handleViewportChange(updatedViewport) {
     setViewport(updatedViewport);
+  }
+
+  function handleChangeMarkerPosition(updatedPosition) {
+    setPinCoordinates({
+      latitude: updatedPosition.lngLat[1],
+      longitude: updatedPosition.lngLat[0],
+    });
   }
 
   return (
@@ -21,6 +35,16 @@ export function Map() {
       {...viewport}
       mapboxApiAccessToken={mapBoxAPIAccessToken}
       onViewportChange={handleViewportChange}
-    />
+    >
+      <Marker
+        {...markerCoordinates}
+        onDragEnd={handleChangeMarkerPosition}
+        offsetTop={-16}
+        offsetLeft={-10}
+        draggable
+      >
+        <Pin />
+      </Marker>
+    </ReactMapGL>
   );
 }
