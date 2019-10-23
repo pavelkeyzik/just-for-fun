f6bd2ebc5975f77f065644026674bb4a827f1e38
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-23 16:09:06 +0300

    Added beatiful form layout for creating new place

--- src/pages/admin/CreateNewPlace.js
+++ src/pages/admin/CreateNewPlace.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import { Input } from '../../components/Input';
+import { Button } from './components/Button';
+
+import './CreateNewPlace.css';
+
 function CreateNewPlace() {
+  function handleAddNewPlace(event) {
+    event.preventDefault();
+  }
+
   return (
     <React.Fragment>
       <div className="Page__short-info">
         <h2>Create new place</h2>
       </div>
+      <section className="CreateNewPlace__page-grid">
+        <form
+          className="CreateNewPlace__form-grid"
+          onSubmit={handleAddNewPlace}
+        >
+          <label>Place name</label>
+          <Input type="text" placeholder="Enter name of new place" />
+          <label>Place address</label>
+          <Input type="text" placeholder="Enter name of new place" />
+          <Button type="submit">Save place</Button>
+        </form>
+        <div className="CreateNewPlace__map-container">
+          Here will be a map...
+        </div>
+      </section>
     </React.Fragment>
   );
 }
