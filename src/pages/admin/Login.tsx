03318d69281946b93af515258f288111317ddffe
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-31 18:10:28 +0300

    Migrated Login component to CSS-modules

--- src/pages/admin/Login.tsx
+++ src/pages/admin/Login.tsx
@@ -3,14 +3,14 @@ import { Card, CardBody, CardHeader, Col, Container, Row } from 'reactstrap';
 
 import LoginForm from './login/LoginForm';
 
-import './Login.css';
+import styles from './Login.module.css';
 
 function Login(): JSX.Element {
   return (
     <Container>
-      <Row className="Login__row">
+      <Row className={styles.row}>
         <Col lg="7">
-          <Card body inverse className="Login__card">
+          <Card body inverse className={styles.card}>
             <CardHeader>
               <h2>Admin Login</h2>
             </CardHeader>
