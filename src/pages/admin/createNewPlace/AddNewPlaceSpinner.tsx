a3d9d8fa874ff8e177640eca087b2af6dfcd3537
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-31 18:14:53 +0300

    Migrated AddNewPlaceSpinner component to CSS-modules

--- src/pages/admin/createNewPlace/AddNewPlaceSpinner.tsx
+++ src/pages/admin/createNewPlace/AddNewPlaceSpinner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Spinner } from 'reactstrap';
 
-import './AddNewPlaceSpinner.css';
+import styles from './AddNewPlaceSpinner.module.css';
 
 interface AddNewPlaceSpinnerProps {
   message?: string;
@@ -11,9 +11,9 @@ export function AddNewPlaceSpinner({
   message,
 }: AddNewPlaceSpinnerProps): JSX.Element {
   return (
-    <div className="AddNewPlaceSpinner__container">
+    <div className={styles.container}>
       <Spinner type="grow" color="success" />
-      <span className="AddNewPlaceSpinner__message">
+      <span className={styles.message}>
         {message || 'Something is happening here...'}
       </span>
     </div>
