b4c2f46fb8c9871f1b8e2331fb73a42aa641809f
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-31 17:36:32 +0300

    Migrated Places component to CSS-modules

--- src/pages/main/Places.tsx
+++ src/pages/main/Places.tsx
@@ -3,13 +3,13 @@ import { Container } from 'reactstrap';
 
 import { PlacesGrid } from './places/PlacesGrid';
 
-import './Places.css';
+import styles from './Places.module.css';
 
 function Places(): JSX.Element {
   return (
     <Container>
-      <main className="Places__main">
-        <h2 className="Places__section-title">Places</h2>
+      <main className={styles.main}>
+        <h2 className={styles.sectionTitle}>Places</h2>
         <PlacesGrid />
       </main>
     </Container>
