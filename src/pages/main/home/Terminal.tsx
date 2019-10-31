2c9a4ecf22b18a7ba8b5cae82a377867ecc8629a
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-31 17:56:27 +0300

    Migrated Terminal component to CSS-modules

--- src/pages/main/home/Terminal.tsx
+++ src/pages/main/home/Terminal.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
 
-import './Terminal.css';
+import styles from './Terminal.module.css';
 
 export function Terminal(): JSX.Element {
   return (
-    <div className="Terminal">
-      <div className="Terminal-header">
-        <div className="Terminal-buttons">
-          <div className="Terminal-button Terminal-button_close" />
-          <div className="Terminal-button Terminal-button_minimize" />
-          <div className="Terminal-button Terminal-button_fullscreen" />
+    <div className={styles.root}>
+      <div className={styles.header}>
+        <div className={styles.buttons}>
+          <div className={classNames([styles.button, styles.close])} />
+          <div className={classNames([styles.button, styles.minimize])} />
+          <div className={classNames([styles.button, styles.fullscreen])} />
         </div>
       </div>
-      <div className="Terminal-content" aria-hidden="true">
+      <div className={styles.content} aria-hidden="true">
         <code>
           <b>~root:</b> wget `React and GraphQL`
           <br />
           <b>~root:</b> node little-bit-fun.js
           <br />
           {'Fun started '}
-          <span className="Terminal-content_success">succesfully</span>
+          <span className={styles.contentSuccess}>succesfully</span>
           {" on your heart's port..."}
         </code>
       </div>
