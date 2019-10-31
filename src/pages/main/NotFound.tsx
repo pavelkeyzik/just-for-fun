d67530809d50257719dc528c55d7e4af71e047b0
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-10-31 17:38:14 +0300

    Migrated NotFound component to CSS-modules

--- src/pages/main/NotFound.tsx
+++ src/pages/main/NotFound.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 
 import { routes } from '../../config';
 
-import './NotFound.css';
+import styles from './NotFound.module.css';
 
 function NotFound(): JSX.Element {
   return (
-    <main className="NotFound__main">
-      <h2 className="NotFound__section-title">404 Page Not Found</h2>
-      <p>
-        {
-          "Sorry! We didn't find anything interesting for you here. Please, click the button below to go home."
-        }
-      </p>
-      <Button tag={Link} to={routes.home} color="primary" outline>
-        Go to home page
-      </Button>
-    </main>
+    <Container>
+      <main className={styles.main}>
+        <h2 className={styles.sectionTitle}>404 Page Not Found</h2>
+        <p>
+          {
+            "Sorry! We didn't find anything interesting for you here. Please, click the button below to go home."
+          }
+        </p>
+        <Button tag={Link} to={routes.home} color="primary" outline>
+          Go to home page
+        </Button>
+      </main>
+    </Container>
   );
 }
 
