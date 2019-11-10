874f4379976f357672e164223d9485dfd620464d
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-11-10 18:40:01 +0000

    Added ability to create production build

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,5 +1,31 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = function() {
   return {
     devtool: 'source-map',
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: true,
+              },
+            },
+          ],
+          include: /\.module\.css$/,
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          exclude: /\.module\.css$/,
+        },
+      ],
+    },
   };
 };
