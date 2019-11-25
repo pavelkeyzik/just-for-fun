b449962c6584bb474d4d6a5f66247476fe5e6c90
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-11-25 18:16:58 +0000

    Added path for contentBase in development evnironment

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = function() {
   return {
     devServer: {
+      contentBase: path.join(__dirname, '../public'),
       historyApiFallback: true,
     },
     module: {
