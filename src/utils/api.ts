5e19c1ed195343e5fce375b60e9a7c2e1b89f9bd
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-11-26 17:45:19 +0300

    Removed unused mocks and changed API url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,21 +1,13 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { SchemaLink } from 'apollo-link-schema';
-import { makeExecutableSchema } from 'graphql-tools';
 
 import { apiBase } from '../config';
-import { schema, resolvers } from './schema';
-import { isProduction } from '../config';
-
-const executableSchema = makeExecutableSchema({ typeDefs: schema, resolvers });
 
 const cache = new InMemoryCache();
-const link = isProduction
-  ? new HttpLink({
-      uri: apiBase,
-    })
-  : new SchemaLink({ schema: executableSchema });
+const link = new HttpLink({
+  uri: apiBase,
+});
 
 export const client = new ApolloClient({
   cache,
