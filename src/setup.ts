b3cf3b4e756f8b08337c320fdeaaf766ac869579
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-11-28 13:42:59 +0300

    Changed import of firebase/app module

--- src/setup.ts
+++ src/setup.ts
@@ -1,8 +1,8 @@
-import { initializeApp, performance } from 'firebase/app';
+import * as firebase from 'firebase/app';
 import 'firebase/performance';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { firebaseConfig } from './config';
 
-initializeApp(firebaseConfig);
-performance();
+firebase.initializeApp(firebaseConfig);
+firebase.performance();
