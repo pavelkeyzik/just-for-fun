b321aad7d62c8d76f10cf5429eb2acb99305a497
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-11-28 13:23:50 +0300

    Added basic Firebase configuration for performance analytics

--- src/config.ts
+++ src/config.ts
@@ -25,3 +25,13 @@ export const routes = {
 
 export const defaultMarkerLat = 53.888019427799776;
 export const defaultMarkerLng = 27.544588765497792;
+
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
