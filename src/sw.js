ba1ee1dfe04f1a35d377cfac0c34e6402c7c9a3a
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-12-03 15:28:47 +0300

    Added cache name details

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,11 @@
+// @ts-nocheck
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: 'just-for-fun',
+  suffix: 'v1',
+});
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
