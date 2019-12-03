c50a813b4faac93b8b7439c4130cba3fdd499f2c
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-12-03 15:28:47 +0300

    Added cache name details

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,14 +1,7 @@
 export function registerServiceWorker(): void {
   if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
     window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('sw.js')
-        .then(registration => {
-          console.log('SW registered: ', registration);
-        })
-        .catch(registrationError => {
-          console.log('SW registration failed: ', registrationError);
-        });
+      navigator.serviceWorker.register('sw.js');
     });
   }
 }
