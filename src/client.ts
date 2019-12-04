e0719ba9f37701947cfe841b32a9641d6b386c55
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-12-04 13:55:40 +0300

    Moved apollo client to the src folder

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
-import { apiBase } from '../config';
+import { apiBase } from './config';
 
 const cache = new InMemoryCache();
 
