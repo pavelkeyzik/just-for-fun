d77670a6093578df68eefd3db0df3d4ce6483f04
Pavel Keyzik <pavel.keyzik@gmail.com>
2019-12-05 11:43:58 +0300

    Added ApolloLink to combine multiple links in application

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { apiBase } from './config';
@@ -21,7 +22,12 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const link = ApolloLink.from([
+  authLink,
+  httpLink,
+]);
+
 export const client = new ApolloClient({
   cache,
-  link: authLink.concat(httpLink),
+  link,
 });
